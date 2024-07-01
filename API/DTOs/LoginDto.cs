e8118d08bfaf162bbe6cd41f0337705b1b17e097
CarlosDMolina <cakimolina86@gmail.com>
2024-06-30 19:04:16 -0500

    End of Section 4 - JWT, Auth, etc.

---
+++ API/DTOs/LoginDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs;
+
+public class LoginDto
+{
+    [Required]
+    public required string UserName { get; set; }
+    [Required]
+    public required string Password { get; set; }
+}
--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,7 +1,14 @@
-﻿namespace API.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Entities;
 
 public class AppUser
 {
     public int Id { get; set; }
+    [Required]
     public required string UserName { get; set; }
+    [Required]
+    public required byte[] PasswordHash { get; set; }
+    [Required]
+    public required byte[] PasswordSalt { get; set; }
 }
